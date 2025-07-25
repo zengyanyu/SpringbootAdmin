64e75beb134d805116d71bd501f3c7b93c455263
1194314874@qq.com <zeng123456>
2025-07-25 16:03:41 +0800

    修改文件

--- AdminService/src/main/java/com/zengyanyu/config/SecurityConfig.java
+++ AdminService/src/main/java/com/zengyanyu/config/SecurityConfig.java
@@ -9,11 +9,6 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().anyRequest().permitAll()
-//                .and().csrf().disable();
-//    }
 
     private String adminContextPath;
 
