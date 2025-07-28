83908a34a831c6c499a2d37bb1b2b598bcbc3b97
1194314874@qq.com <zeng123456>
2025-07-28 16:33:14 +0800

    修改文件

--- AdminService/src/main/java/com/zengyanyu/config/SecurityConfig.java
+++ AdminService/src/main/java/com/zengyanyu/config/SecurityConfig.java
@@ -10,15 +10,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/assets/**", "/actuator/**", "/login", "/instances/**").permitAll()// 不需要认证的地址
-                .anyRequest().authenticated().and().formLogin()
-                .loginPage("/login")
-                .and().logout()
-                .logoutUrl("/logout")
-                .and().httpBasic()
-                .and().csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringAntMatchers("/instances").disable();
+        http.authorizeRequests()//
+                .antMatchers("/login", "/assets/**", "/instances/**").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().loginPage("/login").and()
+                .logout().logoutUrl("/logout").and()
+                .httpBasic().and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers("/instances/**", "/actuator/**");
     }
 
 }
